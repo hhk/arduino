bcb3fdae0183ceeccb1aba5c654f8ef30b53e2b1
James Knowles <jamesk@starforgelabs.com>
2012-05-26 18:39:39 -0600

    (Melissa) I MADE {G A M E  S H O W} LIGHTS!

---
+++ potentiometer_controlled__G_a_m_e__g_A_m_e__g_a_M_e__g_a_m_E__r/potentiometer_controlled__G_a_m_e__g_A_m_e__g_a_M_e__g_a_m_E__r.ino
@@ -0,0 +1,57 @@
+int potpin = 0;
+int val;
+
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  val = analogRead(potpin);
+  val = map(val, 0, 1023, 0, 500);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(val);
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  delay(val_);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  delay(val);
+}
+
