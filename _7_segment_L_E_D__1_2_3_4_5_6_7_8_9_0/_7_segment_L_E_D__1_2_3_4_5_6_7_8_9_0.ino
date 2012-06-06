9fced503d69e6d34e71bf234eeb5b0009518dac4
Melissa Knowles <melissa@hoshiro.com>
2012-06-05 19:59:30 -0600

    finished program

--- _7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0/_7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0.ino
+++ _7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0/_7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0.ino
@@ -12,7 +12,7 @@ void setup()
   pinMode(7, OUTPUT);
 }
 
-void number(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8)
+void number(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7)
 {
   digitalWrite(1, pin1);
   digitalWrite(2, pin2);
@@ -21,23 +21,31 @@ void number(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7
   digitalWrite(5, pin5);
   digitalWrite(6, pin6);
   digitalWrite(7, pin7);
-  digitalWrite(8, pin8);
 }
 
 void loop()
 {
   val = analogRead(potpin);
-  val = map(val, 0, 1023, 0, 1000);
-  
-  number(LOW, HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW);
-  number(HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH, LOW);
-  number(HIGH, HIGH, HIGH, HIGH, HIGH, LOW, HIGH, LOW);
-  number(LOW, HIGH, HIGH, HIGH, LOW, LOW, HIGH, HIGH);
-  number(HIGH, LOW, HIGH, HIGH, HIGH, LOW, HIGH, HIGH);
-  number(HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH);
-  number(HIGH, HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW);
-  number(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH);
-  number(HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH, HIGH);
-  number(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW, HIGH);
+  val = map(val, 0, 1023, 0, 9);
+  if(val == 0);
+    number(HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH);
+  if (val == 1);
+    number(LOW, LOW, LOW, LOW, HIGH, HIGH, LOW);
+  if (val == 2);
+    number(LOW, HIGH, HIGH, HIGH, LOW, HIGH, HIGH);
+ if(val == 3);
+    number(LOW, HIGH, LOW, HIGH, HIGH, HIGH, HIGH);
+ if( val == 4);
+    number(HIGH, HIGH, LOW, LOW, HIGH, HIGH, LOW);
+   if(val == 5); 
+    number(HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH);
+ if( val == 6);
+    number ( HIGH, HIGH, HIGH, HIGH, HIGH, LOW, HIGH);
+ if(val == 7);
+    number( LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH);
+   if(val == 8);
+    number(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH);
+  if(val == 9);
+    number(HIGH, HIGH, LOW, LOW, HIGH, HIGH, HIGH);
 }
 
