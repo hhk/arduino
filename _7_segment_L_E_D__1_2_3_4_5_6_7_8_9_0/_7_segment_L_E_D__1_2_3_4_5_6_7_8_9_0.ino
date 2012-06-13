c6206f6f6d1098ec1156293f26c3233d69eec951
Melissa Knowles <melissa@hoshiro.com>
2012-06-13 14:01:31 -0600

    YAY!!! IT WORKS!!!

--- _7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0/_7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0.ino
+++ _7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0/_7_segment_L_E_D__1_2_3_4_5_6_7_8_9_0.ino
@@ -3,49 +3,69 @@ int val;
 
 void setup()
 {
-  pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
-void number(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7)
+void show_number(int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8)
 {
-  digitalWrite(1, pin1);
-  digitalWrite(2, pin2);
-  digitalWrite(3, pin3);
-  digitalWrite(4, pin4);
-  digitalWrite(5, pin5);
-  digitalWrite(6, pin6);
-  digitalWrite(7, pin7);
+   val = analogRead(potpin);
+   val = map(val, 0, 1023, 0, 9);
+   digitalWrite(2, pin2);
+   digitalWrite(3, pin3);
+   digitalWrite(4, pin4);
+   digitalWrite(5, pin5);
+   digitalWrite(6, pin6);
+   digitalWrite(7, pin7); 
+   digitalWrite(8, pin8);  
 }
 
-void loop()
+ void loop()
+ {
+ if (val == 0)
+ {
+   show_number(HIGH,  LOW, HIGH, HIGH, HIGH, HIGH, HIGH);
+ }
+  if (val == 1)
+  {
+   show_number( LOW,  LOW,  LOW,  LOW, LOW, HIGH,  HIGH);
+  }
+ if (val == 2)
+{ 
+   show_number( LOW, HIGH, HIGH, HIGH,  HIGH, HIGH, LOW);
+}
+if (val == 3)
 {
-  val = analogRead(potpin);
-  val = map(val, 0, 1023, 0, 9);
-  if(val == 0);
-    number(HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH);
-  if (val == 1);
-    number(LOW, LOW, LOW, LOW, HIGH, HIGH, LOW);
-  if (val == 2);
-    number(LOW, HIGH, HIGH, HIGH, LOW, HIGH, HIGH);
- if(val == 3);
-    number(LOW, HIGH, LOW, HIGH, HIGH, HIGH, HIGH);
- if( val == 4);
-    number(HIGH, HIGH, LOW, LOW, HIGH, HIGH, LOW);
-   if(val == 5); 
-    number(HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH);
- if( val == 6);
-    number ( HIGH, HIGH, HIGH, HIGH, HIGH, LOW, HIGH);
- if(val == 7);
-    number( LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH);
-   if(val == 8);
-    number(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH);
-  if(val == 9);
-    number(HIGH, HIGH, LOW, LOW, HIGH, HIGH, HIGH);
+   show_number( LOW, HIGH,  LOW, HIGH, HIGH, HIGH, HIGH);
 }
-
+if (val == 4)
+{
+   show_number(HIGH, HIGH,  LOW,  LOW, LOW, HIGH,  HIGH); 
+}
+if (val == 5)
+{
+   show_number(HIGH, HIGH,  LOW, HIGH, HIGH,  LOW, HIGH); 
+}
+ if (val == 6)
+ {
+   show_number(HIGH, HIGH, HIGH, HIGH, HIGH,  LOW, HIGH); 
+ }
+  if (val == 7)
+   {
+   show_number( LOW,  LOW,  LOW,  LOW, HIGH, HIGH, HIGH); 
+   }
+   if (val == 8)
+   {
+   show_number(HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH); 
+   }
+   if (val == 9)
+   {
+   show_number(HIGH, HIGH,  LOW, HIGH, HIGH, HIGH, HIGH);
+   }
+   
+ }
