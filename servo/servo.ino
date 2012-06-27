e67444df843017e7b0f508736f430ab81261c83f
James Knowles <jamesk@starforgelabs.com>
2012-06-27 00:46:00 -0600

    Good try. I made comments.

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,4 +1,12 @@
 //are this program right?
+
+// *Is* the program right? 
+//
+// * Capitalization is important and must be excatly right. 
+// * Punctuation is important and has specialm meaning. It also has to be exactly right. 
+// servo attach should be servo.attach for example (missing period).
+
+
 #include <servo.h>
 servo servo;
 void setup()
@@ -10,9 +18,12 @@ void setup()
 void loop()
 {
   int sensorValue = analogRead (A5);
+  // We can use map() here to make it from 0 to 180. 
+  // This does the same thing, but map() I think is more understandable. 
   int digitalValue  = (int) (180* sensorValue/1024.0);
   servoWrite(digitalValue);
   serial.print(sensorValue);
+  // strings must be surrounded by double quotes (") so this should be " ".
   serial.print(  );
   serial.println(digitalValue);
 }
