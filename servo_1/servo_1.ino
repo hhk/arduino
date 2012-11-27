4843724f05661af67f1cb41e7ec490adfeb45052
James Knowles <jamesk@starforgelabs.com>
2012-11-26 19:47:56 -0700

    Made a few changes Added Serial.print function (I am having fun with this) Melissa

--- servo_1/servo_1.ino
+++ servo_1/servo_1.ino
@@ -5,6 +5,7 @@ Servo myservo;
 
 void setup()
 {
+  Serial.begin(9600);
   myservo.attach(3,640,2330);
 }
 
@@ -14,4 +15,8 @@ void loop()
   int sensorValue = analogRead(5);
   int digitalValue = map(sensorValue, 0, 1023, 0, 180);
   myservo.write(digitalValue);
+  Serial.print("sensor=");
+  Serial.print(sensorValue);
+  Serial.print(" digitalValue=");
+  Serial.println(digitalValue);
 }
